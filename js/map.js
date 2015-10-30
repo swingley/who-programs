2a45cf05abb8a953a47a6e94a4fe81e4725f2869
swingley <swingley@gmail.com>
2015-10-30 08:48:32 -0700

    Use short description for filter names, changes to how filtering works.

--- js/map.js
+++ js/map.js
@@ -1,16 +1,16 @@
 var programs, programLookup = {}, highlight;
-
+var programTally = {}; // dev...checking country program participation
 var config = {
   features: 'https://extranet.who.int/arcgis/rest/services/GIS/PAU_Programme_Activity_201510/MapServer/0',
   name: 'detailed_2011.CNTRY_TERR',
   programAll: '_v_PAU_Programme_Activity_2.Extra_Information',
   programCount: '_v_PAU_Programme_Activity_2.Programme_Count',
   // programMatcher is a regular expression to match things like:
-  // _v_PAU_Programme_Activity_2.Programme_3_Description
-  //  not:
   // _v_PAU_Programme_Activity_2.Programme_3_ShortDescription
+  //  not:
+  // _v_PAU_Programme_Activity_2.Programme_3_Description
   // _v_PAU_Programme_Activity_2.Programme_3_Activity
-  programMatcher: /Programme\_\d\_Description/
+  programMatcher: /Programme\_\d\_ShortDescription/
 };
 
 var map = L.map('map').setView([30, 0], 2);
@@ -65,7 +65,7 @@ function highlightFeature(e) {
 function buildInfo(props) {
   var programInfo = '';
   if ( props && props.hasOwnProperty(config.name) && props.hasOwnProperty(config.programAll) ) {
-    programInfo = '<h4>WHO Programs:  ' + props[config.name] + '</h4>';
+    programInfo = '<h4>' + props[config.name] + '</h4>';
     programInfo += props[config.programAll];
   } else {
     programInfo = '<h4>WHO Programs:</h4>Click a country.';
@@ -105,14 +105,21 @@ function createLegend(info) {
     }
     filters.addEventListener('click', function(e) {
       if ( e.target.id ) {
+        var selected = getSelectedPrograms(e);
+        console.log('click event listener, selected', selected);
         programs.getLayers().forEach(function(l) {
-          var clicked = programLookup[e.target.id];
-          if ( l.feature.properties[config.programAll].indexOf(clicked) === -1 ) {
-            if ( l._map ) {
-              map.removeLayer(l);
-            } else {
-              map.addLayer(l);
+          var props = l.feature.properties;
+          var show = false;
+          for ( var i = 0; i < selected.length; i++ ) {
+            if ( props.hasOwnProperty(selected[i]) && props[selected[i]] ) {
+              show = true;
+              break;
             }
+          }
+          if ( show ) {
+            map.addLayer(l);
+          } else {
+            map.removeLayer(l);
           }
         });
       }
@@ -122,21 +129,53 @@ function createLegend(info) {
   legend.addTo(map);
 }
 
-function showPrograms(json) {
+function createProgramList(json) {
   var programsList = [];
-  json.features.forEach(function(f) {
+  // Loop through all features.
+  json.features.forEach(function(f, i) {
+    console.log('looping through features', i, f.properties[config.name]);
+    // Loop through all attributes.
     for ( var p in f.properties ) {
+      // Check if the current attribute has program info.
       if ( config.programMatcher.test(p) ) {
         var gram = f.properties[p];
-        if ( gram && gram.indexOf('Adviser') === -1 && programsList.indexOf(gram) === -1 ) {
-          programsList.push(gram);
+        // Check if this attribute corresponds to a program.
+        if ( gram ) {
+          // Create a list of unique programs and lookup object
+          if ( gram.indexOf('Adviser') === -1 && programsList.indexOf(gram) === -1 ) {
+            programLookup['program' + programsList.length] = gram;
+            programLookup[gram] = 'program' + programsList.length;
+            programTally['program' + programsList.length] = [];
+            programsList.push(gram);
+            console.log('\tpushed:  ', gram);
+          }
+          // Add a new attribute to indicate this feature participates in a program.
+          // This attribute is used when filtering (showing/hiding) features.
+          var programId = programLookup[gram];
+          if ( programId ) {
+            f.properties[programId] = true;
+            console.log('\t', f.properties[config.name], programLookup[gram], gram);
+            // all program tally stuff is debug...remove at some point
+            if ( programTally[programId].indexOf(f.properties[config.name]) === -1 ) {
+              programTally[programId].push(f.properties[config.name]);
+            }
+          }
         }
       }
     }
-    console.log('\nprogramsList', programsList);
   });
   console.log('programsList', programsList);
   createLegend(programsList);
+}
+
+function getSelectedPrograms(e) {
+  // Get all the checked check boxes.
+  var checked = document.querySelectorAll('input[type=checkbox]:checked');
+  // Return the IDs
+  var selected = _.map(checked, function(c) {
+    return c.id;
+  });
+  return selected;
 }
 
 // Get country data, add it to the map.
@@ -144,7 +183,7 @@ L.esri.Tasks.query({
   url: config.features
 }).where("1=1").precision(6).run(function(error, countries) {
   console.log('got stuff', countries);
-  showPrograms(countries);
+  createProgramList(countries);
   programs = L.geoJson(countries, {
     onEachFeature: onEachFeature, 
     style: style
